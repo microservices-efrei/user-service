53504d17f4e3356fefea6dd315f03c73bee17829
Louis <roccalouis26@gmail.com>
2024-12-02 12:36:07 +0100

    update docker config

--- src/config/database.js
+++ src/config/database.js
@@ -3,15 +3,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Remplacez ces valeurs par celles de votre base de données
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST || 'localhost', // Utilise 'postgres' dans Docker Compose
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'user_db',
+  logging: false, // Désactive les logs SQL dans la console
 });
-
-sequelize
-  .authenticate()
-  .then(() => console.log('Connexion réussie !'))
-  .catch((err) => console.error('Erreur de connexion :', err));
 
 module.exports = sequelize;
--- src/scripts/sync.js
+++ src/scripts/sync.js
@@ -1,8 +1,10 @@
 // scripts/sync.js
 const sequelize = require('../config/database');
+require('../models/user');
 
 (async () => {
   try {
+    console.log(sequelize.models);
     await sequelize.sync({ force: true }); // force: true => Réinitialise les tables
     console.log('Base de données synchronisée !');
     process.exit();
