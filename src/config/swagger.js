ab4821cabde8d02ca908d891b5979d7e177cac51
Louis <roccalouis26@gmail.com>
2024-12-10 12:44:58 +0100

    add docs swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -4,18 +4,14 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Books Documentation',
+      title: 'Users Documentation',
       version: '1.0.0',
       description: 'API documentation for our application',
     },
     servers: [
       {
-        url: 'http://localhost:8080/api/',
+        url: 'http://localhost:3001/api/',
         description: 'Local development server',
-      },
-      {
-        url: 'https://back-generateur-cv-grp1.onrender.com/api',
-        description: 'Production server',
       },
     ],
   },
